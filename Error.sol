81db424aecbd907d5d86c3c1f2dd4e79ae6200b1
EthanKIM <khg023246@gmail.com>
2023-01-18 03:01:50 +0900

    learn : error

---
+++ Error.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// 이번엔 solidity에서 error에 관한 내용을 알아보겠습니다.
+// solidity에는 기본적으로, 컨트랙트와 상호작용에서 에러가 날경우 3가지의 stance를 취합니다.
+// require, revert, assert
+// 해당 에러가 발생했을때 gas는 refund되고, state가 없데이트 되는 작용이 있다면, Reverted 됩니다.
+
+contract Error {
+  function testRequire(uint _i) public pure {
+    require(_i <= 10, "i must greater than 10");
+
+    // _i 가 10보다 작을때의 코드를 작성하면됨.
+  }
+
+  function testRevert(uint _i) public pure {
+    if (_i > 1) {
+      if (_i > 2) {
+        // revert는 이 함수처럼 분기가 많을때, 조건이 많을때 해당 조건에 해당 하지 않는다면
+        // revert가 나게끔 로직을 구현할때 주로 사용합니다.
+        revert("i must greater than 2");
+      }
+    }
+  }
+  uint public num = 123;
+
+  function testAssert() public view {
+    // assert는 해당 메소드 이후에 조건이 변경되었을때 에러를 감지합니다. Require 은 이전에 감지하는것.
+    // assert는 기본적으로 state variable가 accidently하게 변경되었는지를 판단할때 사용.
+    assert(num == 123);
+  }
+  function testRequire2() public view {
+    require(num == 123, "num must 123");
+  }
+  // foo 함수에서 state variable 인 Num은 변하고 있습니다. 이름 감지한 assert는 오류를 반환합니다.
+  function foo (uint _i) public {
+    // 여기 num 처럼 accidently change가 발생했을 경우, 위의 assert를 다시 호출하면, assert에 걸리게 됨.
+    num += 1;
+    require(_i <10);
+  }
+
+  // custom error => custom error을 사용하면 gas 비를 줄일 수 있음. require, revert보다 훨씬 저렴한 gas
+  // require 문 뒤에 나오는 모든 string 값이 돈이라고 생각하면됨.
+  error MyError(address caller, uint i);
+
+  function testCustomError (uint _i) public view returns(uint){
+    if (_i >10) {
+      revert MyError(msg.sender, _i);
+    }
+    return _i;
+  }
+}
